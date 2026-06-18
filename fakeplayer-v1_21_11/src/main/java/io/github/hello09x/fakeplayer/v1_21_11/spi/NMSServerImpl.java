3c9b0f42dc0c9e4dc89f7db95996cfb219b64013
EndlessPixel <3785267350@qq.com>
2026-06-18 21:40:21 +0800

    Sync commit https://github.com/xiplugin/FakePlayer/commit/1f2d4fbc7aa30abe8aa48ea4e5df27a72d39d11d from xiplugin/FakePlayer repo

--- fakeplayer-v1_21_11/src/main/java/io/github/hello09x/fakeplayer/v1_21_11/spi/NMSServerImpl.java
+++ fakeplayer-v1_21_11/src/main/java/io/github/hello09x/fakeplayer/v1_21_11/spi/NMSServerImpl.java
@@ -5,9 +5,12 @@ import io.github.hello09x.devtools.core.utils.WorldUtils;
 import io.github.hello09x.fakeplayer.api.spi.NMSServer;
 import io.github.hello09x.fakeplayer.api.spi.NMSServerPlayer;
 import lombok.Getter;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ClientInformation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.ProblemReporter;
+import net.minecraft.world.level.storage.TagValueInput;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.craftbukkit.v1_21_R7.CraftServer;
@@ -27,12 +30,21 @@ public class NMSServerImpl implements NMSServer {
 
     @Override
     public @NotNull NMSServerPlayer newPlayer(@NotNull UUID uuid, @NotNull String name) {
+        var server = new NMSServerImpl(Bukkit.getServer()).getHandle();
         var handle = new ServerPlayer(
                 new NMSServerImpl(Bukkit.getServer()).getHandle(),
                 new NMSServerLevelImpl(WorldUtils.getMainWorld()).getHandle(),
                 new GameProfile(uuid, name),
                 ClientInformation.createDefault()
         );
+        server.getPlayerList().playerIo.load(handle.nameAndId()).ifPresent(nbt -> {
+            var valueInput = TagValueInput.create(
+                    ProblemReporter.DISCARDING,
+                    server.registryAccess(),
+                    nbt
+            );
+            handle.load(valueInput);
+        });
         return new NMSServerPlayerImpl(handle.getBukkitEntity());
     }
 }
